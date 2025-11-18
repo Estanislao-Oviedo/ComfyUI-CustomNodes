f5d5346923594622e97b12c32a487ad3629987bc
Estanislao Oviedo <estanislaojoviedo@gmail.com>
2025-11-18 20:13:14 -0300

    Wan V2V nodes

--- __init__.py
+++ __init__.py
@@ -6,6 +6,8 @@ from .nodes.load_image_folder import LoadImageFolder
 from .nodes.make_batch_from_single_image import MakeBatchFromSingleImage
 from .nodes.region_conditioning_nodes import RegionConditionSpecPct, RegionConditionSpecPx, RegionConditionMerge
 from .nodes.attention_couple import AttentionCouple
+from .nodes.combine_video_clips import CombineVideoClips
+from .nodes.seamless_join_video_clips import WanVideoVaceSeamlessJoin
 import os
 import nodes
 
@@ -17,6 +19,8 @@ NODE_CLASS_MAPPINGS = {
     "RegionConditionSpecPx": RegionConditionSpecPx,
     "RegionConditionMerge": RegionConditionMerge,
     "AttentionCouple": AttentionCouple,
+    "CombineVideoClips": CombineVideoClips,
+    "WanVideoVaceSeamlessJoin": WanVideoVaceSeamlessJoin,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -26,6 +30,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "RegionConditionSpecPx": "Region Condition Spec (Pixels)",
     "RegionConditionMerge": "Region Condition Merge",
     "AttentionCouple": "Attention Couple",
+    "CombineVideoClips": "Combine Video Clips",
+    "WanVideoVaceSeamlessJoin": "Wan Video Vace Seamless Join",
 }
 
 # Add JS extension directory for frontend
